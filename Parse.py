35cd180e4308328e4951e8736663012a2bb88656
sina <sadeghesfahani.sina@gmail.com>
2021-05-21 21:44:58 +0430

    new method implimentet to get inner paranteses

--- Parse.py
+++ Parse.py
@@ -5,40 +5,41 @@ class Parse:
         self.validate = True
         self.inner_parts = list()
         self.operators = list()
+        self.paranteses = list()
         self.scam()
 
     def scam(self):
         counter_open = 0
         counter_close = 0
-        inner = str()
-        open_check = False
+        inner = dict()
+        open_check = [False, 0]
         # (self.text[let].isnumberic() and not self.text[let+1].isnumeric())
+        starter_paranteses = list()
+
         for let in range(len(self.text)):
             if self.text[let] == "(":
-                open_check = True
-                self.opens.append(let)
-                counter_open += 1
+                starter_paranteses.append(let)
             elif self.text[let] == ")":
-                if open_check:
-                    self.closed.append(let)
-                    counter_close += 1
-                    open_check = False
-                    self.inner_parts.append(inner)
-                    inner = str()
-                else:
-                    self.validate = False
-            else:
-                if let < len(self.text):
-                    if self.text[let] in "+-*/":
-                        self.operators.append(let)
-
-                if open_check:
-                    inner += self.text[let]
-        if counter_open == counter_close and self.validate:
-            pass
-        else:
-            print("wrong")
+                try:
+                    self.paranteses.append([starter_paranteses[len(starter_paranteses)-1], let])
+                    starter_paranteses.pop()
+                except:
+                    print("format wrong")
+        blocks=[set(range(block[0],block[1]+1)) for block in self.paranteses]
+        final_blocks=blocks.copy()
+        for block in blocks:
+            for I in range(len(blocks)):
+                if block !=blocks[I]:
+                    test=block.intersection(blocks[I])
+                    if block.intersection(blocks[I])== block:
+                        final_blocks.remove(blocks[I])
+        final_blocks1 = [[sorted(list(block))[0], sorted(list(block))[len(block)-1]] for block in final_blocks]
+        print(final_blocks1)
 
 
-par1 = Parse("35*(4578+8754)+56log(545/23)")
-print(par1.inner_parts)
+par1 = Parse("35*(4578(25*6)+8754)+56log(545/23)")
+print(par1.paranteses)
+
+
+
+# [4,9]
